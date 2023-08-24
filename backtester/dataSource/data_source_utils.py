f711107a30640e01c89b36db894ac0eceee10595
ktchan33GBC <113069483+ktchan33GBC@users.noreply.github.com>
2023-08-24 11:11:30 -0400

    Update data_source_utils.py

--- backtester/dataSource/data_source_utils.py
+++ backtester/dataSource/data_source_utils.py
@@ -23,17 +23,17 @@ def getCookieForYahoo(instrumentId):
     cookie = req.cookies['B']
     pattern = re.compile('.*"CrumbStore":\{"crumb":"(?P<crumb>[^"]+)"\}')
 
-    # for line in txt.splitlines():
-    #     m = pattern.match(line.decode("utf-8"))
-    #     if m is not None:
-    #         crumb = m.groupdict()['crumb']
-    #         crumb = crumb.replace(u'\\u002F', '/')
-    # return cookie, crumb  # return a tuple of crumb and cookie
+    for line in txt.splitlines():
+        m = pattern.match(line.decode("utf-8"))
+        if m is not None:
+            crumb = m.groupdict()['crumb']
+            crumb = crumb.replace(u'\\u002F', '/')
+    return cookie, crumb  # return a tuple of crumb and cookie
 
 
-# def downloadFileFromYahoo(startDate, endDate, instrumentId, fileName, event='history'):
-#     logInfo('Downloading %s' % fileName)
-#     cookie, crumb = getCookieForYahoo(instrumentId)
+def downloadFileFromYahoo(startDate, endDate, instrumentId, fileName, event='history'):
+    logInfo('Downloading %s' % fileName)
+    cookie, crumb = getCookieForYahoo(instrumentId)
     start = int(mktime(startDate.timetuple()))
     end = int(mktime(endDate.timetuple()))
     url = 'https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=%s&crumb=%s' % (instrumentId, start, end, event, crumb)
