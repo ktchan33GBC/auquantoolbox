f77c85cec41598e3f0ac19d94e36c041e889ddb1
ktchan33GBC <113069483+ktchan33GBC@users.noreply.github.com>
2023-08-23 21:49:02 -0400

    Update data_source_utils.py

--- backtester/dataSource/data_source_utils.py
+++ backtester/dataSource/data_source_utils.py
@@ -8,30 +8,36 @@ import re
 from time import mktime as mktime
 from itertools import groupby
 
+def getHeadersForYahoo():
+    return {'Connection': 'keep-alive',
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
+              AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
+
+
 
 def getCookieForYahoo(instrumentId):
     """Returns a tuple pair of cookie and crumb used in the request"""
     url = 'https://finance.yahoo.com/quote/%s/history' % (instrumentId)
-    req = requests.get(url)
+    req = requests.get(url, headers=getHeadersForYahoo())
     txt = req.content
     cookie = req.cookies['B']
     pattern = re.compile('.*"CrumbStore":\{"crumb":"(?P<crumb>[^"]+)"\}')
 
-    for line in txt.splitlines():
-        m = pattern.match(line.decode("utf-8"))
-        if m is not None:
-            crumb = m.groupdict()['crumb']
-            crumb = crumb.replace(u'\\u002F', '/')
-    return cookie, crumb  # return a tuple of crumb and cookie
+    # for line in txt.splitlines():
+    #     m = pattern.match(line.decode("utf-8"))
+    #     if m is not None:
+    #         crumb = m.groupdict()['crumb']
+    #         crumb = crumb.replace(u'\\u002F', '/')
+    # return cookie, crumb  # return a tuple of crumb and cookie
 
 
-def downloadFileFromYahoo(startDate, endDate, instrumentId, fileName, event='history'):
-    logInfo('Downloading %s' % fileName)
-    cookie, crumb = getCookieForYahoo(instrumentId)
+# def downloadFileFromYahoo(startDate, endDate, instrumentId, fileName, event='history'):
+#     logInfo('Downloading %s' % fileName)
+#     cookie, crumb = getCookieForYahoo(instrumentId)
     start = int(mktime(startDate.timetuple()))
     end = int(mktime(endDate.timetuple()))
     url = 'https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=%s&crumb=%s' % (instrumentId, start, end, event, crumb)
-    data = requests.get(url, cookies={'B': cookie})
+    data = requests.get(url, cookies={'B': cookie}, headers=getHeadersForYahoo())
     with open(fileName, 'wb') as f:
         f.write(data.content)
         return True
